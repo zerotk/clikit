3ef0671ac9cb7401563b477db1199735a6c156df
Alexandre Andrade <kaniabi@gmail.com>
2016-02-21 17:01:53 -0300

    Applying zerotk.uniform to generate auxiliary files. Initial (empty) code files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# clickit
-Command Line Interface KIT.
+[![PyPI](https://img.shields.io/pypi/v/zerotk.clikit.svg?style=flat-square)]()
+[![Travis](https://img.shields.io/travis/zerotk/clikit.svg?style=flat-square)]()
+[![Coveralls](https://img.shields.io/coveralls/zerotk/clikit.svg?style=flat-square)]()
+
+# zerotk.clikit
+
+A modern Command Line Interface library.
+
+```python
+import zerotk.clikit
+```
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='zerotk.clikit',
+    use_scm_version=True,
+
+    author='Alexandre Andrade',
+    author_email='kaniabi@gmail.com',
+
+    url='https://github.com/zerotk/clikit',
+
+    description='A modern Command Line Interface library.',
+    long_description='''A modern Command Line Interface library.''',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    include_package_data=True,
+
+    packages=['zerotk', 'zerotk.clikit'],
+    namespace_packages=['zerotk'],
+
+    keywords=['command line', 'cli'],
+
+    install_requires=['six'],
+    setup_requires=['setuptools_scm', 'pytest-runner'],
+    tests_require=['coverage', 'pytest'],
+)
